c2c89518e4c62ee33cc76b8a4a92d5581ecb9632
José Domingos <domliraa@gmail.com>
2024-05-03 14:55:55 -0300

    feat: add api calls

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,20 +15,10 @@ import storm from "../assets/storm.svg";
 import halfSun from "../assets/half-sun.svg";
 import sun from "../assets/Sun.svg";
 import chart from "../assets/Chart.svg";
-import {
-  ChangeEvent,
-  ChangeEventHandler,
-  MouseEventHandler,
-  useEffect,
-  useState,
-} from "react";
-
-import debounce from "lodash.debounce";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useDebounce } from "@/hook/useDebounce";
 
-const API_KEY = "a444fe22d218251c383739249655853e";
-
-interface WeatherDate {
+interface WeatherData {
   id: number;
   main: {
     feels_like: number;
@@ -54,57 +44,72 @@ interface WeatherDate {
 }
 
 export default function Main() {
-  const [searchCity, setSearchCity] = useState("Salvador");
-  const [currentCity, setCurrentCity] = useState("Salvador");
-  const [state, setState] = useState("");
-  const [weatherData, setweatherData] = useState<WeatherDate>();
-  const debounceSearch = useDebounce(searchCity);
+  const [searchCity, setSearchCity] = useState("");
+  const [currentCity] = useState("Recife");
+  const [UF, setUF] = useState("");
+  const debounceSearch = useDebounce(!searchCity ? currentCity : searchCity);
+  const [weatherData, setWeatherData] = useState<WeatherData>();
 
   useEffect(() => {
     fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${debounceSearch}&limit=1&appid=${API_KEY}`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${debounceSearch}&limit=1&appid=${process.env.NEXT_PUBLIC_API_KEY}`
     )
       .then((response) => {
-        //return response.json();
         return response.json();
       })
       .then((dataResponse) => {
-        console.log(dataResponse);
-        setState(dataResponse[0].state);
+        if (!dataResponse[0].state) return;
+        getUF(dataResponse[0].state);
+
         fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${dataResponse[0].lat}&lon=${dataResponse[0].lon}&appid=${API_KEY}`
+          `https://api.openweathermap.org/data/2.5/weather?lat=${dataResponse[0].lat}&lon=${dataResponse[0].lon}&appid=${process.env.NEXT_PUBLIC_API_KEY}&lang=pt-br`
         )
           .then((response) => {
             return response.json();
           })
           .then((weatherData) => {
-            setweatherData(weatherData);
-            console.log(weatherData);
+            setWeatherData(weatherData);
           });
       });
-    // console.log(debounceSearch)
   }, [debounceSearch]);
 
-  /* function onChangedCity(event: ChangeEvent<HTMLInputElement>) {
-    event.preventDefault()
+  function onChangedCity(event: ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
 
-    setSearchCity(event.target.value)
+    setSearchCity(event.target.value);
+  }
 
-    const teste = debounce(onChangedCity, 1000)
+  function getUF(estado: string) {
+    try {
+      if (estado.split(" ").length > 1) {
+        const stateSplit = estado.split(" ");
+        const statesplitedwordnumber = stateSplit.length;
+        const UFState =
+          stateSplit[0].charAt(0) +
+          stateSplit[statesplitedwordnumber - 1].charAt(0);
+        setUF(UFState);
+      } else {
+        const UFstate = estado.substring(0, 2).toLocaleUpperCase();
+        setUF(UFstate);
+      }
+    } catch (error) {}
+  }
 
-  } */
-  //console.log(debounceSearch)
+  function divisionForTen(number: number) {
+    return number / 10;
+  }
+
 
   return (
     <main className="flex flex-col space-y-36 px-20 justify-center items-center">
-      <div>
+      <div className="px-4 py-2 rounded-md bg-transparent/5">
+        <label className="font-bold text-lg text-gray-200">Cidade : </label>
         <input
           type="text"
+          className="bg-transparent outline-none text-gray-200"
           value={searchCity}
-          onChange={(e) => setSearchCity(e.target.value)}
-          /* onChange={onChangedCity} */
+          onChange={onChangedCity}
         />
-        <span>{searchCity}</span>
       </div>
       <div className="flex gap-8">
         <div className="w-[480px] min-h-[480px] shadow-2xl bg-right-bottom bg-cover bg-[url('../assets/background.svg')] bg-no-repeat flex flex-col justify-between p-3 rounded-lg">
@@ -118,24 +123,23 @@ export default function Main() {
           <div className="flex justify-end items-center">
             <MapPinIcon size={24} fill="#C2BFF4" color="#9D99E4" />
             <span className="text-[#C2BFF4] text-sm font-semibold">
-              {weatherData?.name}, {state?.substring(0, 2).toLocaleUpperCase()}
+              {weatherData?.name}, {UF}
             </span>
           </div>
           <div className="flex items-center justify-center flex-col gap-1">
             <div className="flex ml-5">
               <strong className="text-8xl text-white">
-                {(Number(weatherData?.main.temp) / 10).toFixed()}
+                {(Number(weatherData?.main.temp) / 10).toFixed(0)}
               </strong>
               <span className="text-zinc-200 font-semibold text-2xl">°C</span>
             </div>
 
             <div className="flex justify-center items-center gap-1 text-2xl">
               <span className="font-medium text-zinc-100">
-              {(Number(weatherData?.main.temp_min) / 10).toFixed(1)}°
-                
+                {(Number(weatherData?.main.temp_min) / 10).toFixed(0)}°
               </span>
               <span className="font-medium text-zinc-300">
-              {(Number(weatherData?.main.temp_max) / 10).toFixed(1)}°
+                {(Number(weatherData?.main.temp_max) / 10).toFixed(0)}°
               </span>
             </div>
           </div>
@@ -148,7 +152,7 @@ export default function Main() {
                 </span>
                 <div className="flex items-baseline gap-1">
                   <span className="font-bold text-lg">
-                    {Math.floor(Number(weatherData?.wind.speed))}
+                    {(Number(weatherData?.wind.speed) / 10).toFixed(0)}
                   </span>
                   <span className="font-bold text-sm text-[#DAD8F7]">km/h</span>
                 </div>
@@ -162,7 +166,7 @@ export default function Main() {
                 </span>
                 <div className="flex items-baseline gap-1">
                   <span className="font-bold text-lg">
-                    {weatherData?.main.humidity}
+                    {(Number(weatherData?.main.humidity) / 10).toFixed(0)}
                   </span>
                   <span className="font-bold text-sm text-[#DAD8F7]">%</span>
                 </div>
@@ -237,11 +241,6 @@ export default function Main() {
                 </span>
               </div>
               <Image src={chart} alt="" sizes="auto" />
-              {/* <Image src={line} alt="" width={216} height={122} /> */}
-              {/* <div className="flex w-full justify-between">
-              <span>06:12</span>
-              <span>18:52</span>
-            </div> */}
             </div>
           </div>
           <div className="w-full p-10 flex gap-3 bg-[#6D67D0]/60 rounded-md">
